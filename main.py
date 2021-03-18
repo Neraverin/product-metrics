8d7d1d189ab8d47497ed8bb6e2f11de3ab619f85
Salykin Vladimir <neraverin@gmail.com>
2021-03-18 15:41:15 +0300

    Simple metrics test

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import os
 from wallarm_api import WallarmAPI
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_env():
+    api_UUID = os.environ.get('WALLARM_UUID')
+    api_secret = os.environ.get('WALLARM_SECRET')
+    return api_UUID, api_secret
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    api = WallarmAPI()
-    api.client_id()
-    print_hi('PyCharm')
+    api_UUID, api_secret = get_env()
+    api = WallarmAPI(api_UUID, api_secret)
+    client = api.clients_api.get_client()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    clients = api.clients_api.get_clients()
+    for client in clients:
+        subscription = api.billing_api.get_subscription(client.id)
+
