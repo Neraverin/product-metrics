e406c325a720612e28b8c00d50a06c44964d6875
Salykin Vladimir <neraverin@gmail.com>
2021-03-31 17:53:11 +0300

    Implement base uploading metrics to Google Sheets

--- product_metrics/core.py
+++ product_metrics/core.py
@@ -1,5 +1,7 @@
 import os
 import humanize
+import pygsheets as pygsheets
+import datetime as dt
 
 from models import apiconnection
 from metrics.nsm import NorthStarMetric
@@ -12,11 +14,25 @@ def get_eu_connection():
 
 
 if __name__ == '__main__':
-    EU_connection = get_eu_connection()
+    google_client = pygsheets.authorize(service_file='credentials.json')
+    spreadsheet = google_client.open('Product metrics')
+    working_sheet = spreadsheet.sheet1
 
+    EU_connection = get_eu_connection()
     metrics = [NorthStarMetric(EU_connection)]
 
-    dates = [[2021, 3]]
+    dates = [[2021, 1], [2021, 2], [2021, 3]]
+    month_counter = 2
     for year, month in dates:
-        for metric in metrics:
-            print(metric.name, 'in', year, ',', month, ' = ', humanize.intword(metric.value(year=year, month=month)))
+        working_sheet.cell((1, month_counter)).value = humanize.naturaldate(dt.date(year, month, 1))
+
+        for i in range(len(metrics)):
+            metric_value = metrics[i].value(year=year, month=month)
+            print(metrics[i].name, 'in', year, ',', month, ' = ',
+                  humanize.intword(metric_value))
+            working_sheet.cell((i + 2, 1)).value = metrics[i].name
+            working_sheet.cell((i + 2, month_counter)).value = metric_value
+
+        month_counter += 1
+
+
