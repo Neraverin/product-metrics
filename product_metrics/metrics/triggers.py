3d7338a3a56a397fa3d279d28e6db842d76af32c
Otmakhova <ulianaotmakhova@gmail.com>
2021-08-27 09:41:32 +0000

    Added metrics for triggers templates

--- product_metrics/metrics/triggers.py
+++ product_metrics/metrics/triggers.py
@@ -1,36 +1,49 @@
-from logging import logThreads
-
-from pydantic.utils import truncate
 from .base_type import BaseType
 from .base_metric import BaseMetric
 from .metric_helper import MetricHelper
 from wallarm_api import WallarmAPI
+
 from collections import Counter
 
 
 class TriggersMetric(BaseType):
-    FILTER_IDS = [("Trigger by attack_type", 59, "attack_type"), 
-                  ("Trigger by pool", 60,"pool"), 
-                  ("Trigger by ip_address", 61,"ip_address"), 
-                  ("Trigger by domain", 62,"domain"), 
-                  ("Trigger by response_status", 63,"response_status"), 
-                  ("Trigger by user_role", 64,"user_role"), 
-                  ("Trigger by target", 65,"target"), 
-                  ("Trigger by url", 66, "url"), 
-                  ("Trigger by hint_tag", 67, "hint_tag"), 
+    FILTER_IDS = [("Trigger by attack_type", 59, "attack_type"),
+                  ("Trigger by pool", 60, "pool"),
+                  ("Trigger by ip_address", 61, "ip_address"),
+                  ("Trigger by domain", 62, "domain"),
+                  ("Trigger by response_status", 63, "response_status"),
+                  ("Trigger by user_role", 64, "user_role"),
+                  ("Trigger by target", 65, "target"),
+                  ("Trigger by url", 66, "url"),
+                  ("Trigger by hint_tag", 67, "hint_tag"),
                   ("Trigger by threshold", 68, "threshold")]
 
-    USER_ROLES = [("Trigger by superadmin", 69, "superadmin"),
-                  ("Trigger by partner_admin", 70, "partner_admin"),
-                  ("Trigger by client roles (except superadmin and partner_admin)", 71, "client")]
+    USER_ROLES = [("Trigger created by superadmin", 69, "superadmin"),
+                  ("Trigger created by partner_admin", 70, "partner_admin"),
+                  ("Trigger created by client roles (except superadmin and partner_admin)", 71, "client")]
 
-    def get_filters(self, all_triggers):
-        client_filters = []
-        for i in all_triggers:
-            for j in i.filters:
-                client_filters.append(j.id)
+    TEMPLATES = [("Trigger template for requests", 86, 'bruteforce_started'),
+                 ("Trigger template for attack vectors", 87, 'vector_attack'),
+                 ("Trigger template for created user", 88, 'user_created'),
+                 ("Trigger template for attacks", 89, 'attacks_exceeded'),
+                 ("Trigger template for hits", 90, 'hits_exceeded'),
+                 ("Trigger template for incidents", 91, 'incidents_exceeded'),
+                 ("Trigger template for blacklisted IP", 92, 'blacklist_ip_added'), ]
 
-        return dict(Counter(client_filters))
+    TEMPLATES_BY_CLIENT = [("Trigger template for requests by client",
+                            93, 'bruteforce_started'),
+                           ("Trigger template for attack vectors by client",
+                            94, 'vector_attack'),
+                           ("Trigger template for created user by client",
+                            95, 'user_created'),
+                           ("Trigger template for attacks by client",
+                            96, 'attacks_exceeded'),
+                           ("Trigger template for hits by client",
+                            97, 'hits_exceeded'),
+                           ("Trigger template for incidents by client",
+                            98, 'incidents_exceeded'),
+                           ("Trigger template for blacklisted IP by client",
+                            99, 'blacklist_ip_added')]
 
     def get_triggers(self):
         all_triggers = []
@@ -40,31 +53,48 @@ class TriggersMetric(BaseType):
 
         return all_triggers
 
+    def get_filters(self, all_triggers):
+        client_filters = []
+        for i in all_triggers:
+            for j in i.filters:
+                client_filters.append(j.id)
+
+        return dict(Counter(client_filters))
+
+    def get_templates(self, triggers):
+        if isinstance(triggers[0], dict):
+            templates = [i['template_id'] for i in triggers]
+        else:
+            templates = [i.template_id for i in triggers]
+        return dict(Counter(templates))
+
     def get_triggers_by_creator(self):
-        partner_admin_triggers = 0
-        superadmin_triggers = 0
-        client_triggers = 0
+        partner_admin_triggers = []
+        superadmin_triggers = []
+        client_triggers = []
+
         for client in MetricHelper().real_clients:
-            logs = WallarmAPI().audit_log_api.get_log({"action": "create", 
-                                                        "object_type": "triggers.trigger", 
-                                                        "subject_clientid": client.id}, limit=1000)
+            logs = WallarmAPI().audit_log_api.get_log({"action": "create",
+                                                       "object_type": "triggers.trigger",
+                                                       "subject_clientid": client.id}, limit=1000)
 
             for log in logs.objects:
-                users = WallarmAPI().users_api.get_users({"id": log.object_userid})
+                users = WallarmAPI().users_api.get_users(
+                    {"id": log.object_userid})
                 if len(users.users) == 0:
                     continue
                 else:
                     user = users.users[0]
 
                 if 'superadmin' in user.permissions:
-                    superadmin_triggers += 1
+                    superadmin_triggers.append(log.entity)
                 elif 'partner_admin' in user.permissions:
-                    partner_admin_triggers += 1
+                    partner_admin_triggers.append(log.entity)
                 else:
-                    client_triggers += 1
+                    client_triggers.append(log.entity)
 
-        result = {'superadmin': superadmin_triggers, 
-                  'partner_admin': partner_admin_triggers, 
+        result = {'superadmin': superadmin_triggers,
+                  'partner_admin': partner_admin_triggers,
                   "client": client_triggers}
         return result
 
@@ -94,7 +124,7 @@ class TriggersMetric(BaseType):
         def value(self) -> int:
             if self.filter_id in self.filters:
                 return self.filters[self.filter_id]
-            else: 
+            else:
                 return 0
 
     class CountTriggersByCreator(BaseMetric):
@@ -104,13 +134,41 @@ class TriggersMetric(BaseType):
             self.triggers_by_creator = triggers_by_creator
 
         def value(self) -> int:
-            return self.triggers_by_creator[self.role]
+            return len(self.triggers_by_creator[self.role])
+
+    class CountTriggersTemplates(BaseMetric):
+        def __init__(self, templates, name, row, template):
+            super().__init__(name, row)
+            self.templates = templates
+            self.template = template
+
+        def value(self) -> int:
+            if self.template in self.templates:
+                return self.templates[self.template]
+            else:
+                return 0
+
+    class CountTriggersTemplatesByClient(BaseMetric):
+        def __init__(self, templates_by_client, name, row, template):
+            super().__init__(name, row)
+            self.templates_by_client = templates_by_client
+            self.template = template
+
+        def value(self) -> int:
+            if self.template in self.templates_by_client:
+                return self.templates_by_client[self.template]
+            else:
+                return 0
 
     def collect_metrics(self) -> list:
         triggers = self.get_triggers()
         filters = self.get_filters(triggers)
         triggers_by_creator = self.get_triggers_by_creator()
+        templates = self.get_templates(triggers)
+        templates_by_client = self.get_templates(triggers_by_creator["client"])
 
-        return [self.CountTotalTriggers(triggers), self.CountActiveTriggers(triggers)] +[
+        return [self.CountTriggersTemplatesByClient(templates_by_client, *t) for t in self.TEMPLATES_BY_CLIENT] + [
+                self.CountTriggersTemplates(templates, *t) for t in self.TEMPLATES] + [
+                self.CountTotalTriggers(triggers), self.CountActiveTriggers(triggers)] + [
                 self.CountFiltersByType(filters, *t) for t in self.FILTER_IDS] + [
                 self.CountTriggersByCreator(triggers_by_creator, *t) for t in self.USER_ROLES]
